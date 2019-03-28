a8db95d1d5f76b7f3db1c4741ac405bc0c36b899
jiawentao <wentaojia2014@163.com>
2019-03-28 17:38:44 +0800

    change sln config

---
+++ ImageProcessing/ImageProcessing/header/stdafx.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#pragma comment(lib, "opencv_calib3d343.lib")
+#pragma comment(lib, "opencv_core343.lib")
+#pragma comment(lib, "opencv_features2d343.lib")
+#pragma comment(lib, "opencv_flann343.lib")
+#pragma comment(lib, "opencv_highgui343.lib")
+#pragma comment(lib, "opencv_imgcodecs343.lib")
+#pragma comment(lib, "opencv_imgproc343.lib")
+#pragma comment(lib, "opencv_ml343.lib")
+#pragma comment(lib, "opencv_objdetect343.lib")
+#pragma comment(lib, "opencv_photo343.lib")
+#pragma comment(lib, "opencv_shape343.lib")
+#pragma comment(lib, "opencv_stitching343.lib")
+#pragma comment(lib, "opencv_superres343.lib")
+#pragma comment(lib, "opencv_video343.lib")
+#pragma comment(lib, "opencv_videoio343.lib")
+#pragma comment(lib, "opencv_videostab343.lib")
